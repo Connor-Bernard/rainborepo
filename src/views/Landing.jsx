2bb136e3609a4c442ba478810033ccb9d3157f15
David Camacho <noedavidcamacho@berkeley.edu>
2024-04-20 15:13:56 -0700

    Fixing main page

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style/Landing.css';
+import styles from './style/Landing.module.css';
+import classname from 'classnames';
 
 export default function LandingPage() {
     return (
+        
         <>
-            <div class="task-header">
+        
+            <div className={classname(styles.taskHeader)}>
                 <div>
-                    <Link to="/" class="button" id="home-button">Home</Link>
+                    <Link to="/" className={classname(styles.button)} id={styles.homeButton}>Home</Link>
                 </div>
 
-                {/* todo: figure out how to center title? */}
+                {/* todo: figure out how to center title? */}   
                 <div>
                     <h1>rainborepo</h1>
                 </div>
             </div>
 
-            <div class="container">
-                <Link to="/prompt" class="button red">PROMPT</Link>
-                <Link to="/survey" class="button blue">SURVEY</Link>
+            <div className={classname(styles.container)}>
+                <Link to="/prompt" className={classname(styles.button, styles.red)}>PROMPT</Link>
+                <Link to="/survey" className={classname(styles.button, styles.blue)}>SURVEY</Link>
             </div>
         </>
     )
