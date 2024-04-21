e369c147ca1bc82769cf1b9ee4287c72fccae554
David Camacho <noedavidcamacho@berkeley.edu>
2024-04-21 16:38:15 -0700

    basic desc for landing

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -5,23 +5,28 @@ import classname from 'classnames';
 
 export default function LandingPage() {
     return (
-        
+
         <>
-        <title>Landing Page</title>
+            <title>Landing Page</title>
             <div className={classname(styles.taskHeader)}>
                 <div>
                     <Link to="/" className={classname(styles.button)} id={styles.homeButton}>Home</Link>
                 </div>
 
-                {/* todo: figure out how to center title? */}   
+                {/* todo: figure out how to center title? */}
                 <div>
                     <h1>rainborepo</h1>
                 </div>
             </div>
 
             <div className={classname(styles.container)}>
-                <Link to="/prompt" className={classname(styles.button, styles.red)}>PROMPT</Link>
-                <Link to="/survey" className={classname(styles.button, styles.blue)}>SURVEY</Link>
+                <Link to="/prompt" className={classname(styles.button, styles.red, styles.largeFont)}>PROMPT <br></br>
+                    <span className={styles.smallFont}>Explore our media treasure trove with Rainborepo AI! Unearth the perfect recommendation tailored just for you by effortlessly searching through our extensive database.</span>
+                </Link>
+                <Link to="/survey" className={classname(styles.button, styles.blue, styles.largeFont)}>SURVEY
+                    <br></br>
+                    <span className={styles.smallFont}>Hitting a mental block? Simply breeze through a short survey and let our AI generate tailored recommendations just for you!</span>
+                </Link>
             </div>
         </>
     )
